54fd42ba8106bc5c2641327db17d51f5c1270889
Alexander-Guacan <adguacan@espe.edu.ec>
2023-02-08 20:54:21 -0500

    Imprimiendo listado de ubicaciones para seleccion por parte del usuario

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 from Grafo import Grafo
 
+def imprimir_ubicaciones(ubicaciones: list[str]) -> None:
+    i = 1
+    # Imprimir cada ubicacion como un listado con su indice asociado
+    for ubicacion in ubicaciones:
+        print(f"{i}.- {ubicacion}")
+        # Incrementar el indice de ubicacion
+        i += 1
+
 def main() -> None:
     # Ubicaciones por donde puede transitar la ambulancia
-    ubicaciones = ["Rumiñahui", "La bota", "Quinta", "Hospital de Solca", "Llano chico", "La luz", "San Isidro del Inca", "Zambiza"]
+    ubicaciones = ["Ruminiahui", "La bota", "Quinta", "Hospital de Solca", "Llano chico", "La luz", "San Isidro del Inca", "Zambiza"]
+    # Imprimir ubicaciones como menu
+    imprimir_ubicaciones(ubicaciones)
     # Generando grafo
     grafo = Grafo([[5, 3, 1], [0], [3, 4], [], [2, 3, 7], [0], [3, 7], [4, 6]])
     # Ingresando nodo inicial y final a recorrer en el grafo
@@ -13,7 +23,7 @@ def main() -> None:
     i = 1
     # Imprimiendo todos los caminos posibles desde el nodo inicial hasta el nodo final
     print(f"Caminos posibles desde {nodo_inicial} hasta {nodo_objetivo}: ")
-    for camino in grafo.caminos(nodo_inicial, nodo_objetivo):
+    for camino in grafo.caminos(nodo_inicial - 1, nodo_objetivo - 1):
         ruta = str()
         # Asociando el numero de cada nodo con el nombre de su ubicacion
         for nodo in camino:
